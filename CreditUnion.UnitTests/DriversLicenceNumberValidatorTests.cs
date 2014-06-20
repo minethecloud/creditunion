0e025da4b98e65392ceb9a0346091c90c81784cf
samuelmensah <samuelnmensah@live.com>
2014-06-19 23:56:33 -0700

    added unit tests

---
+++ CreditUnion.UnitTests/DriversLicenceNumberValidatorTests.cs
@@ -0,0 +1,39 @@
+﻿using CreditUnioin.Loans;
+using NUnit.Framework;
+
+namespace CreditUnion.UnitTests
+{
+    [TestFixture]
+    [Category("Drivers License Tests")]
+    public class DriversLicenceNumberValidatorTests
+    {
+        [Test]
+        public void ShouldBeInvalidWhenAllSpaces()
+        {
+            var sut = new DriversLicenseValidator();
+            Assert.That(sut.IsValid("   "),Is.False);
+        }
+        [Test]
+        public void ShouldBeInvalidWhenBlack()
+        {
+            var sut = new DriversLicenseValidator();
+            Assert.That(sut.IsValid(""), Is.False);
+        }
+         
+        [Test]
+        public void ShouldBeInvalidWhenMoreThan8Characters()
+        {
+            var sut = new DriversLicenseValidator();
+            const string driversLicenseNumber = "T12343434433";
+            Assert.That(sut.IsValid(driversLicenseNumber), Is.False);
+        }
+
+        [Test]
+        public void ShouldBeValidWhen8DigitsAndStartsWithLetter()
+        {
+            var sut = new DriversLicenseValidator();
+            const string driversLicenseNumber = "A5522123";
+            Assert.That(sut.IsValid(driversLicenseNumber), Is.True);
+        }
+    }
+}
